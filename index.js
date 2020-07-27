3b69f472de835c164f7a20810a6477627fc47b41
olegs7771 <olegs7771@gmail.com>
2020-07-27 22:39:44 +0300

    ok

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const fs = require("fs");
+const http = require("http");
 
-const file = fs.readFileSync("./txt/input.txt", "utf-8");
+///FILES
+//Blocking Synchronous way
+// const text = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(text);
+// const textOut = ` This about avocado :${text}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("file written");
 
-console.log(file);
+//Non Bloking ---- Asynchronous way to write the file
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error ❗️");
+//   console.log("data1", data1);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log("data2", data2);
+//     fs.writeFile(
+//       "./txt/final.txt",
+//       `${data2}\n${data1}\n 💇 `,
+//       "utf-8",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("finished ");
+//       }
+//     );
+//   });
+// });
+// console.log("will read file 👍 ");
+
+////SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
